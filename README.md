7c7b326a75d95aba8b2d6235b2344c98d5e791b9
Lucas <lucas.ribeiro711@gmail.com>
2021-10-20 13:52:49 -0300

    Js features

---
+++ README.md
@@ -0,0 +1,22 @@
+<img src="https://github.com/lukinhas711/finance-control-application/blob/main/README/devF.png" title="Cabeçalho, dev.finances">
+
+<h1 align="center">Dev.finance$</h1>
+
+<p align="center">Essse projeto se trata de uma aplicação de controle financeiro desenvolvido durante a <a href="https://app.rocketseat.com.br/node/maratona-discover-edicao-01" target="_blank">Maratona discover da Rocketseat</a>.</p>
+
+<h2 align="center">Layout mobile da aplicação</h2>
+
+<div align="center">
+  <a href="#"><img src="https://github.com/lukinhas711/finance-control-application/blob/main/README/dev%20finance%20mobile.png" title="screenshot dversão mobile do layout"></a>
+</div>
+
+
+<h2 align="center">Layout desktop da aplicação</h2>
+
+<div align="center">
+  <a href="#"><img src="https://github.com/lukinhas711/finance-control-application/blob/main/README/dev%20finance.png" title="screenshot dversão desktop do layout"></a>
+</div>
+
+<h4 align="center"> 
+	🚧  Aplicação: Em construção...  🚧
+</h4>
---
+++ scripts/script.js
@@ -0,0 +1,155 @@
+//pegar o modal no html e adicionar a classe active para abrir e remover para fechar
+const Modal = {
+  toggle(){
+    let overlay = document.querySelector('.modal-overlay').classList.toggle('active')
+  }
+}
+
+const Transactions = {
+  all: [
+    {
+      description:'Luz',
+      amount: -50000,
+      date: '23/01/21'
+    },
+    {
+      description:'website',
+      amount: 500000,
+      date: '23/01/21'
+    },
+    {
+      description:'Aluguel',
+      amount: -200000,
+      date: '23/01/21'
+    }
+  ],
+
+  add(transaction){
+    Transactions.all.push(transaction)
+    App.reload()
+  },
+
+  remove(index){
+    Transactions.all.splice(index, 1)
+    App.reload()
+  },
+  //somar entradas
+  incomes(){
+    let income = 0
+    //pegar todas as transações
+    //para cada transação,
+    Transactions.all.forEach(transaction => {
+      //se ela for maior que zero, somar a uma variavel e 
+      if (transaction.amount > 0){
+        income += transaction.amount
+      }
+    })
+    //retornar a variavel
+    return income
+  },
+  //calcular saidas 
+  expenses(){
+    let expense = 0
+    //pegar todas as transações
+    //para cada transação,
+    Transactions.all.forEach(transaction => {
+      //se ela for menor que zero, somar a uma variavel e 
+      if (transaction.amount < 0){
+        expense += transaction.amount
+      }
+    })
+    //retornar a variavel
+    return expense
+  },
+  //remover das entradas o valor das saidas
+  total(){
+    //pegar os valores positivos e negativos e somar
+    return Transactions.expenses() + Transactions.incomes()
+  }
+  //então terei o total
+}
+
+//pegar meu objeto do JS e injetar no HTML
+
+const DOM = {
+  transactionContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index){
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLtransaction(transaction)
+    DOM.transactionContainer.appendChild(tr)
+  },
+
+  innerHTMLtransaction(transaction){
+    const incomeExpense = transaction.amount > 0 ? "income" : "expense"
+    const amountValue = Utils.formatCurrency(transaction.amount)
+    const html = `
+    <td class="description">${transaction.description}</td>
+    <td class="${incomeExpense}">${amountValue}</td>
+    <td class="date">${transaction.date}</td>
+    <td><img src="./assets/minus.svg" alt="Remover Transaçâo"></td>`
+
+    return html
+  },
+
+  updateBalance(){
+    document.querySelector('#incomeDisplay').innerHTML = Utils.formatCurrency(Transactions.incomes())
+    document.querySelector('#expenseDisplay').innerHTML = Utils.formatCurrency(Transactions.expenses())
+    document.querySelector('#totalDisplay').innerHTML = Utils.formatCurrency(Transactions.total())
+  },
+
+  clearTransactions(){
+    DOM.transactionContainer.innerHTML = ''
+  }
+}
+
+const Utils = {
+  formatCurrency(value){
+    const signal = Number(value) < 0 ? '-' : ''
+    value = String(value).replace(/\D/g, '')
+    value = Number(value) / 100
+    value = value.toLocaleString('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    return signal + value
+  }
+}
+
+const Form = {
+  validateFields(){
+    console.log('validar os dados')
+  },
+  formatData(){
+    console.log('formatar os dados')
+  },
+
+  submit(event){
+    event.preventDefault()
+    //verificar se todas as informações foram preenchidas
+    Form.validateFields()
+    //formatar os dados e salvar
+    Form.formatData()
+    //salvar
+    //apagar os dados do formulario 
+    //fechar o modal
+    //atualizar a aplicação
+  }
+}
+
+const App = {
+  init(){
+    Transactions.all.forEach(transaction => {
+      DOM.addTransaction(transaction)
+    })
+    
+    DOM.updateBalance()
+  },
+  reload(){
+    DOM.clearTransactions()
+    App.init()
+  }
+}
+
+App.init()
